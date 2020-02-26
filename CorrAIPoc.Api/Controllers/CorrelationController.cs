554484fd739fab088680aecfda14a07aab13af58
Vamsi TP <vamsitp@microsoft.com>
2020-02-26 11:21:14 +0530

    Renamed API-Route

--- CorrAIPoc.Api/Controllers/CorrelationController.cs
+++ CorrAIPoc.Api/Controllers/CorrelationController.cs
@@ -13,33 +13,43 @@
     using Microsoft.Extensions.Logging;
 
     [ApiController]
-    [Route("[controller]")]
-    public class WeatherForecastController : ControllerBase
+    [Route("api")]
+    public class CorrelationController : ControllerBase
     {
         private static readonly string[] Summaries = new[]
         {
             "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
         };
 
-        private readonly ILogger<WeatherForecastController> logger;
+        private readonly ILogger<CorrelationController> logger;
         private readonly IConfiguration configuration;
 
-        public WeatherForecastController(ILogger<WeatherForecastController> logger, IConfiguration configuration)
+        public CorrelationController(ILogger<CorrelationController> logger, IConfiguration configuration)
         {
             this.logger = logger;
             this.configuration = configuration;
         }
 
         [HttpGet]
-        public async Task<IEnumerable<WeatherForecast>> Get([FromQuery]string message)
+        public async Task<IEnumerable<WeatherForecast>> Get()
         {
-            var body = string.IsNullOrWhiteSpace(message) ? DateTime.Now.Ticks.ToString() : message;
-            this.logger.LogInformation(body);
+            return await Process(DateTime.Now.Ticks.ToString());
+        }
+
+        [HttpGet("{message}")]
+        public async Task<IEnumerable<WeatherForecast>> Get([FromRoute]string message)
+        {
+            return await Process(message);
+        }
+
+        private async Task<IEnumerable<WeatherForecast>> Process(string message)
+        {
+            this.logger.LogInformation(message);
             var connectionString = configuration.GetValue<string>("SBConn");
             var sbClient = new QueueClient(connectionString, "inputq");
-            var sbMessage = new Message(Encoding.UTF8.GetBytes(body)) { SessionId = body };
+            var sbMessage = new Message(Encoding.UTF8.GetBytes(message)) { SessionId = message };
             await sbClient.SendAsync(sbMessage);
-            this.logger.LogInformation($"{body} - sbMessage.Diagnostic-Id = {sbMessage.UserProperties["Diagnostic-Id"]} / currActivity.TraceId = {Activity.Current?.TraceId.ToString() ?? string.Empty}");
+            this.logger.LogInformation($"{message} - sbMessage.Diagnostic-Id = {sbMessage.UserProperties["Diagnostic-Id"]} / currActivity.TraceId = {Activity.Current?.TraceId.ToString() ?? string.Empty}");
 
             var rng = new Random();
             return Enumerable.Range(1, 5).Select(index => new WeatherForecast
--- CorrAIPoc.Api/Startup.cs
+++ CorrAIPoc.Api/Startup.cs
@@ -1,17 +1,10 @@
 namespace CorrAIPoc.Api
 {
-    using System;
-    using System.Collections.Generic;
-    using System.Linq;
-    using System.Threading.Tasks;
     using Microsoft.AspNetCore.Builder;
     using Microsoft.AspNetCore.Hosting;
-    using Microsoft.AspNetCore.HttpsPolicy;
-    using Microsoft.AspNetCore.Mvc;
     using Microsoft.Extensions.Configuration;
     using Microsoft.Extensions.DependencyInjection;
     using Microsoft.Extensions.Hosting;
-    using Microsoft.Extensions.Logging;
 
     public class Startup
     {
@@ -25,7 +18,7 @@ namespace CorrAIPoc.Api
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            // services.AddApplicationInsightsTelemetry();
+            services.AddApplicationInsightsTelemetry();
             services.AddControllers();
         }
 
